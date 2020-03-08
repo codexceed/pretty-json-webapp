dc340e44e4e365f506d5db6d4417c4aeecd31468
codexceed <sarthak.xceed@gmail.com>
2020-03-08 12:32:10 +0530

    WIP: dynamic content

--- json_app/routes.py
+++ json_app/routes.py
@@ -2,9 +2,19 @@ from flask import render_template, request
 
 from json_app import app
 from json_app.constants import HOME_TITLE
+from json_app.parse_json import prettify
 
 
-@app.route('/pretty-json')
+@app.route('/')
 def index() -> str:
+    """Pretty-json webpage endpoint."""
     x = request.args
     return render_template('prettyjson.html', title=HOME_TITLE)
+
+
+@app.route('/prettify-json', methods=['POST', 'GET'])
+def prettify_json() -> str:
+    """Return prettified json text."""
+    json_input = request.form.get('json-input')
+    json_output = prettify(text=json_input)
+    return render_template('prettyjson.html', title=HOME_TITLE, json_output=json_output)
